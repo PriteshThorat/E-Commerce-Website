bd6c25cfe9a5d875ac91c85f646e3a991f40cd4a
PriteshThorat <141409170+PriteshThorat@users.noreply.github.com>
2024-08-17 00:41:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cart = document.getElementById("cart");
 const products = document.getElementById("products");
+const header = document.getElementById("header");
 
 let cartBuy;
 let cartRemove;
@@ -10,67 +11,52 @@ cartBackground.id = "cart-background";
 document.body.appendChild(cartBackground);
 const cartElement = document.getElementById("cart-background");
 
-const productsObj = {
-  img : [
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/j/b/n/-original-imahyuhfzvybhaat.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/g/b/x/-original-imagtt4h4ptmxgwn.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/s/9/i/m6-pro-5g-mzb0eqjin-poco-original-imags3e7dazavyje.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/b/m/g/-original-imagxaqtqng2hpxn.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/i/k/l/edge-50-fusion-pb300001in-motorola-original-imahywzpfd2jh9ep.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/4/y/b/-original-imahyzygycuyg3mq.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/i/k/k/c65-mzb0g8rin-poco-original-imagw6gyhhu7hur5.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/q/y/m/-original-imagz7f9hzrahd2z.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/k/g/j/t3x-5g-v2338-vivo-original-imahyyzaqhgwzfup.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/v/c/2/-original-imahfsy9xyk5fujz.jpeg?q=70",
-    "https://rukminim2.flixcart.com/image/200/200/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70"
-  ],
-  name: [
-    "Realme P1 5G",
-    "vivo T2 Pro 5g",
-    "Poco M6 Pro 5G",
-    "Redmi 12 5G",
-    "Moto Edge 50 Fusion",
-    "Realme P1 Pro 5G",
-    "Poco C65",
-    "Nothing Phone 2a",
-    "Vivo t3x 5G",
-    "Infinix Nota 40 Pro",
-    "iPhone 15"
-  ],
-  price: [
-    "14,999",
-    "20,999",
-    "9,249",
-    "9,999",
-    "20,999",
-    "20,999",
-    "6,799",
-    "22,999",
-    "12,499",
-    "15,999",
-    "63,999"
-  ],
-  add: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-};
+const qrElement = document.createElement("div");
+document.body.appendChild(qrElement);
+qrElement.className = "qr-div";
+qrElement.id = "qr";
+
+const imagesURL = [
+  "https://i.imgur.com/LuWzBCD.jpg",
+  "https://i.imgur.com/5AaNff4.jpg",
+  "https://i.imgur.com/mCUfRde.jpg"
+];
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json(); 
+        const { categories, products } = data;
+        productFn(categories, products);
+        addAndRemove(products);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+}
+
+fetchData('https://gist.githubusercontent.com/PriteshThorat/e7c837a6d6d044a81f2cd65b7ec4f198/raw/170488a99b5c77ecbf64419d32760211202d4d55/ecommerce.json');
 
 const cartObj = {
+  id: [],
   name: [],
   price: [],
   add: []
 };
 
-const productFn = () => {
-  const { img, name, price} = productsObj;
-  name.forEach((el, i) => {
+const productFn = (category, data) => {
+  data.forEach((el, i) => {
     products.innerHTML += `
     <div class="product-div">
-      <img class="product-img" src="${img[i]}">
-      <p class="product-name">${el}</p>
-      <p class="product-price">₹${price[i]}</p>
+      <img class="product-img" src="${el.category_id === 1 ? imagesURL[0] : el.category_id === 2 ? imagesURL[1] : imagesURL[2]}">
+      <p class="product-name">${el.name}</p>
+      <p class="product-price">$${el.price}</p>
       <div class="item-add">
-        <button class="product-add" id="add-${i}">+</button>
-        <span class="product-add-value">${productsObj.add[i]}</span>
-        <button class="product-sub" id="sub-${i}">-</button>
+        <button class="product-add" id="add-${el.id}">+</button>
+        <span class="product-add-value">0</span>
+        <button class="product-sub" id="sub-${el.id}">-</button>
       </div>
     </div>
     `;
@@ -83,7 +69,7 @@ const totalAmount = () => {
   let total = 0;
   const { price, add } = cartObj;
   price.forEach((el, i) => {
-    total += parseInt(el.replace(/,/g, "")) * add[i];
+    total += el * add[i];
   });
   return total.toLocaleString();
 };
@@ -92,7 +78,6 @@ const cutAll = () => {
   cartObj.name = [];
   cartObj.price = [];
   cartObj.add = [];
-  productsObj.add = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
   const productAddValue = document.querySelectorAll(".product-add-value");
   productAddValue.forEach((el) => {
@@ -106,12 +91,8 @@ const cutAll = () => {
 };
 
 const cut = (index) => {
-  let replaceIndex = productsObj.name.indexOf(cartObj.name[index]);
-  console.log(replaceIndex);
-  productsObj.add[replaceIndex] = 0;
-
   const productAddValue = document.querySelectorAll(".product-add-value");
-  productAddValue[replaceIndex].textContent = 0;
+  productAddValue[index].textContent = 0;
 
   cartObj.add.splice(index, 1);
   cartObj.price.splice(index, 1);
@@ -123,12 +104,36 @@ const cut = (index) => {
   eventListener();
 };
 
+const buy = () => {
+  if (totalAmount() == 0) {
+    alert("Your Cart is Empty!");
+    return;
+  }
+  
+  cartElement.style.display = "none";
+  header.style.display = "none";
+
+  const qr = document.getElementById("qr");
+  qr.innerHTML = `
+  <img class="buy-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTuEIzrAnoE6-6TrLFKG3kdUsNZ05IMmE-0w&s">
+  <p class="buy-text">Your Total Payment is ₹${totalAmount()}</p>
+  `;
+}
+
 const eventListener = () => {
   if (clicked && cartObj.price.length !== 0) {
     
     cartBuy.forEach((el, i) => {
         el.addEventListener("click", () => {
+          buy();
+          afterBuy();
           cut(el.id);
+          document.addEventListener("mousemove", () => {
+            const qr = document.getElementById("qr");
+            qr.innerHTML = "";
+            cartElement.style.display = "flex";
+            header.style.display = "flex";
+          });
         });
       });
       
@@ -140,7 +145,14 @@ const eventListener = () => {
     }
 
     totalBuy.addEventListener("click", () => {
+      buy();
       cutAll();
+      document.addEventListener("mousemove", () => {
+        const qr = document.getElementById("qr");
+        qr.innerHTML = "";
+        cartElement.style.display = "flex";
+        header.style.display = "flex";
+      });
     });
 };
 
@@ -150,19 +162,18 @@ const cartDisplay = () => {
   <div id="cart-div"></div>
   <div class="total-div">
     <p class="total-text">Total:</p>
-    <p class="total-amount">₹ ${totalAmount()}</p>
+    <p class="total-amount">$ ${totalAmount()}</p>
     <button id="total-buy">Buy All</button>
   </div>
   `;
 
-  const { name, price, add } = cartObj;
-
+  const { name, price, add, id } = cartObj;
   const cartDiv = document.getElementById("cart-div");
   name.forEach((el, i) => {
     cartDiv.innerHTML += `
     <div class="cart-child">
       <p class="cart-name">${el}</p>
-      <span class="cart-price">₹${price[i]}</span>
+      <span class="cart-price">$${price[i]}</span>
       <span class="cart-add">${add[i]} Items</span>
       <div class="cart-btn">
         <button class="cart-buy" id="${i}">Buy</button>
@@ -175,6 +186,53 @@ const cartDisplay = () => {
   cartBuy = document.querySelectorAll(".cart-buy");
   cartRemove = document.querySelectorAll(".cart-remove");
   totalBuy = document.getElementById("total-buy");
+};
+
+const addAndRemove = (data) => {
+  const productAdd = document.querySelectorAll(".product-add");
+  const productSub = document.querySelectorAll(".product-sub");
+  const productAddValue = document.querySelectorAll(".product-add-value");
+
+  data.forEach((element) => {
+    productAdd.forEach((el, i) => {
+      el.addEventListener("click", () => {
+        if (el.id == `add-${element.id}`) {
+          if (!cartObj.id.includes(element.id)) {
+            cartObj.name.push(element.name);
+            cartObj.price.push(element.price);
+            cartObj.id.push(element.id);
+            cartObj.add.push(0);
+          }
+          let addIndex = cartObj.name.indexOf(element.name);
+          cartObj.add[addIndex]++;
+          productAddValue[i].textContent = cartObj.add[addIndex];
+        }
+    });
+  });
+
+  productSub.forEach((el, i) => {
+    el.addEventListener("click", () => {
+      if (el.id == `sub-${element.id}`) {
+        if (!cartObj.id.includes(element.id)) {
+        alert("You Have Not Choosen Any Item!");
+        } else {
+        let removeIndex = cartObj.name.indexOf(element.name);
+
+        if (cartObj.add[removeIndex] === 1) {
+          cartObj.add.splice(removeIndex, 1);
+          cartObj.name.splice(removeIndex, 1);
+          cartObj.id.splice(removeIndex, 1);
+          cartObj.price.splice(removeIndex, 1);
+          productAddValue[i].textContent = 0;
+        } else {
+          cartObj.add[removeIndex]--;
+          productAddValue[i].textContent = cartObj.add[removeIndex];
+        }
+      }
+      }
+    });
+  });
+  });
 };
 
 cart.addEventListener("click", () => {
@@ -193,48 +251,4 @@ cart.addEventListener("click", () => {
     cartDisplay();
     eventListener();
   }
-});
-
-window.addEventListener("load", () => {
-  productFn();
-
-  const productAdd = document.querySelectorAll(".product-add");
-  const productSub = document.querySelectorAll(".product-sub");
-  const productAddValue = document.querySelectorAll(".product-add-value");
-
-  productAdd.forEach((el, i) => {
-    el.addEventListener("click", () => {
-      if (productsObj.add[i] === 0) {
-        cartObj.name.push(productsObj.name[i]);
-        cartObj.price.push(productsObj.price[i]);
-        cartObj.add.push(productsObj.add[i]);
-      }
-
-      let addIndex = cartObj.add.indexOf(productsObj.add[i]);
-      cartObj.add[addIndex]++;
-      productsObj.add[i]++;
-      productAddValue[i].textContent = productsObj.add[i];
-    });
-  });
-
-  productSub.forEach((el, i) => {
-    el.addEventListener("click", () => {
-      if (productsObj.add[i] === 0) {
-        alert("You Have Not Choosen Any Item!");
-      } else {
-        let removeIndex = cartObj.name.indexOf(productsObj.name[i]);
-
-        if (productsObj.add[i] === 1) {
-          
-          cartObj.add.splice(removeIndex, 1);
-          cartObj.name.splice(removeIndex, 1);
-          cartObj.price.splice(removeIndex, 1);
-        }
-
-        cartObj.add[removeIndex]--;
-        productsObj.add[i]--;
-        productAddValue[i].textContent = productsObj.add[i];
-      }
-    });
-  });
 });
