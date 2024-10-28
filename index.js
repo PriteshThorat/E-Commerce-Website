18927231f7feb9d9e054535ea9b8aa8e9aa90bfd
thoratpritesh1 <thoratpritesh1@gmail.com>
2024-10-28 21:14:29 +0000

    1st Step

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
+require('dotenv').config();
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(cors()); // Enable CORS
-
-// Endpoint that sends the secret as plain text
-app.get('/api/secret', (req, res) => {
-    const mySecret = process.env.USER_KEY;
-    res.send(mySecret); // Send as plain text
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+//console.log('Environment Variables:', process.env); // Log all env variables
+const mySecret = process.env.MY_SECRET;
+console.log(`My secret is: ${mySecret}`);
